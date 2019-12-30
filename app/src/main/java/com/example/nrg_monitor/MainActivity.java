d6f3337ea72223890cf0ad81a36b89b7f7869dee
Dima <it15111@uom.edu.gr>
2019-12-30 14:05:54 +0200

    finalized local db insertion and checks, might replace with mySQL db and spring REST webservice

--- app/src/main/java/com/example/nrg_monitor/MainActivity.java
+++ app/src/main/java/com/example/nrg_monitor/MainActivity.java
@@ -1,15 +1,15 @@
 package com.example.nrg_monitor;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
