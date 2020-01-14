a4c8165d107a17c2e561e38ffccbc662e847566a
Dima <it15111@uom.edu.gr>
2020-01-14 09:00:16 +0200

    small additions to main activity

--- app/src/main/java/com/example/nrg_monitor/main/app/MainActivity.java
+++ app/src/main/java/com/example/nrg_monitor/main/app/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.nrg_monitor.main.app;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.DropBoxManager;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private DrawerLayout drawer;
     private SharedPreferences mSharedPreferences ;
+    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +40,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        TextView navigation_username_text = (TextView)headerView.findViewById(R.id.username_text);
+        navigation_username_text.setText(username);
+        TextView navigation_email_text = (TextView)headerView.findViewById(R.id.email_text);
+        navigation_email_text.setText(mSharedPreferences.getString(getApplicationContext().getResources().getString(R.string.logged_in_user_email),""));
+        new GetUsernameFromDB().execute(navigation_email_text.getText().toString());
 
         navigationView.setNavigationItemSelectedListener(this);
 
@@ -43,6 +53,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        TextView navigation_username_text = (TextView)headerView.findViewById(R.id.username_text);
+        navigation_username_text.setText(username);
     }
 
     @Override
@@ -71,6 +90,40 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             super.onBackPressed();
         }
 
+    }
+
+    private class GetUsernameFromDB extends AsyncTask<String,Void,String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            DbRequestHandler dbRequestHandler = new DbRequestHandler();
+
+            String usernameReturned = dbRequestHandler.getUsernameFromDB(strings[0]);
+
+            return usernameReturned;
+
+        }
+
+
+
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            username = s;
+            mSharedPreferences.edit().putString(getApplicationContext().getResources().getString(R.string.logged_in_user),username).apply();
+            TextView username_view = findViewById(R.id.username_text);
+            username_view.setText(s);
+
+
+        }
     }
 
 
